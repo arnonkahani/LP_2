b40d7e91d8669404ebf12d1a1104e2e32d4c938b
arnonkahani <arnon.kahani@gmail.com>
2018-04-14 00:10:12 +0300

    Working for easy examples

--- ex2.pl
+++ ex2.pl
@@ -17,6 +17,11 @@ nonogramPuzzle(easy,4,nonogram(Rows,Cols,ColsBlocks,RowsBlocks)):- % 2 solutions
     ColsBlocks=[[2],[1,1],[2],[2,4],[1,1,2],[1,1,1,1],[2,2]],
     RowsBlocks=[[2],[1,1],[1,1],[2],[2,1],[1,2,2],[4,1],[3]].
 
+nonogramPuzzle(medium,3,nonogram(Rows,Cols,ColsBlocks,RowsBlocks)):-
+    Rows = 10,
+    Cols = 10,
+    ColsBlocks = [[1, 3, 3], [2, 3, 1], [1, 1, 1], [6], [5, 3], [2, 1, 3], [4, 3], [5, 3], [4, 4], [3, 4]],
+    RowsBlocks = [[3, 6], [1, 6], [1, 1, 4], [5, 3], [2, 2, 1], [3, 2, 2], [2, 4], [1, 7], [1, 3, 3], [2, 1]].
 % utils
 
 transpose([], []).
@@ -33,98 +38,79 @@ lists_firsts_rests([[F|Os]|Rest], [F|Fs], [Os|Oss]) :-
         lists_firsts_rests(Rest, Fs, Oss).
 
 
-nonogram_list([],C,C).
-
-nonogram_list([1|T],C,Acc) :-
-    Accn is Acc + 1,
-    nonogram_list(T,C,Accn).
-
-nonogram_list([0|T],C,Acc) :-
-    nonogram_list(T,C,Acc).
-
-
-nonogram_matrix(N,M,Matrix,OnesR,OnesC) :-
-    length(Matrix,N),
-    nonogram_matrix(M,Matrix,OnesR),
-    transpose(Matrix,Ts),
-    nonogram_matrix(N,Ts,OnesC).
-
-nonogram_matrix(_,[],[]).
-
-nonogram_matrix(N,[Mh|Mt],[Oh|Ot]) :-
-    length(Mh,N),
-    nonogram_list(Mh,Oh,0),
-    nonogram_matrix(N,Mt,Ot).
-
-
-sum_list([], 0).
-
-sum_list([H|T], Sum) :-
-   sum_list(T, Rest),
-   Sum is H + Rest.
-
-get_ones([H|T],I,C) :-
-    is_list(H),
-    sum_list(H,Sum),
-    append(I,[Sum],Is),
-    get_ones(T,Is,C).
-
-get_ones([H|T],I,C) :-
-    append(I,[H],Is),
-    get_ones(T,Is,C). 
-
-get_ones([],I,I).
-
 % end utils
 
 % Task 1
 
+line_solution([],[],0).
 
+line_solution([Dh|Dt],[],C) :-
+    Dh == C,
+    line_solution(Dt,[],0).
 
-line_solution([Sh|St],C,D,Acc) :-
-    Sh == 1,
+line_solution(D,[1|St],C) :-
     Cn is C + 1,
-    line_solution(St,Cn,D,Acc).
+    line_solution(D,St,Cn).
 
-line_solution([Sh|St],C,D,Acc) :-
-    Sh == 0,
+line_solution([Dh|Dt],[0|St],C) :-
+    C \== 0,
+    C == Dh,
+    line_solution(Dt,St,0).
+
+line_solution(D,[0|St],C) :-
     C == 0,
-    line_solution(St,0,D,Acc).
+    line_solution(D,St,0).
 
-line_solution([Sh|St],C,D,Acc) :-
-    Sh == 0,
-    C \== 0,
-    append(Acc,[C],Dn),
-    line_solution(St,0,D,Dn).
 
-line_solution([],0,D,D).
+is_nonogram(0,_,[],[]).
 
-line_solution([],C,D,Acc) :-
-    C \== 0,
-    append(Acc,[C],Dn),
-    line_solution([],0,D,Dn).
-
-line_solution(S,D) :-
-    line_solution(S,0,D,[]).
-
-is_nonogram(0,[],[]).
-
-is_nonogram(M,[Rh|Rt],[Sh|St]) :-
-    line_solution(Sh,Rh),
-    Ms is M - 1,
-    is_nonogram(Ms,Rt,St).
+is_nonogram(NumOfRows,NumOfCols,[Rh|Rt],[Sh|St]) :-
+    length(Sh,NumOfCols),
+    line_solution(Rh,Sh,0),
+    Ns is NumOfRows - 1,
+    is_nonogram(Ns,NumOfCols,Rt,St).
 
 is_nonogram(nonogram(N,M,ColData,RowData),Solution) :-
-    length(ColData,M),
-    length(RowData,N),
-    get_ones(RowData,[],OnesR),
-    get_ones(ColData,[],OnesC),
-    nonogram_matrix(N,M,Solution,OnesR,OnesC),
+    length(Solution,N),
     transpose(Solution,Ts),
-    is_nonogram(N,RowData,Solution),
-    is_nonogram(M,ColData,Ts).
+    is_nonogram(N,M,RowData,Solution),
+    is_nonogram(M,N,ColData,Ts).
+
+% Task 2
+nonogram_solve(nonogram(N,M,ColData,RowData), Solution):-
+	construct_matrix(N,M,RowData,Solution),
+    transpose(Solution,Ts),
+    is_nonogram(M,N,ColData,Ts).
+	
+construct_matrix(N,M,[Rh|Rt],[Sh|St]):-
+	N > 0,
+    length(Sh, M), 
+    insert_hint(Rh, Sh),
+	Nn is N - 1,
+	construct_matrix(Nn,M,Rt,St).
+
+construct_matrix(0,_,[],[]).
+
+	
+insert_hint([Rh1,Rh2|Rt], Solution):-
+	length(H, Rh1), 
+	fill_with_k(H,1,Rh1),
+	append(H, [0|N], Solution), 
+	insert_hint([Rh2|Rt], N).
 
 
-nonogram_solve(nonogram(N,M,ColData,RowData), Solution) :-
-    is_nonogram(nonogram(N,M,ColData,RowData),Solution).
+insert_hint([R], Solution):-
+	length(H, R), 
+	fill_with_k(H, 1, R),
+	append(H, N, Solution),
+    length(N,Nl), 
+	fill_with_k(N, 0, Nl).
 
+
+insert_hint(RowData, [0|Solution]):-
+	insert_hint(RowData, Solution).
+	
+fill_with_k([], _, 0).
+fill_with_k([H|T], H, N) :- 
+    Nn is N - 1,
+    fill_with_k(T, H, Nn).
